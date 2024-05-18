aea5ac1494ca09e543f9cf1b42fd2847122709f4
JesusMartinezClavel <jmcvalles@gmail.com>
2024-05-18 19:08:48 +0200

    FEAT -> Created first Angular project for learning

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,11 +14,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'myAngular' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('myAngular');
-  });
+  // it(`should have the 'myAngular' title`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app).toEqual('myAngular');
+  // });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,60 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [RouterOutlet],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   title = 'myAngular';
+// }
+
+// @Component({
+//   selector: 'app-root',
+//   template: `Hello {{city}}, {{1+1}}`,
+//   styles: `:host {color:red}`,
+//   standalone: true
+// })
+// export class AppComponent {
+//   city = 'Valencia'
+// }
+
+@Component({
+  selector: 'app-user',
+  template: `
+    Username: {{ username }}
+  `,
+  standalone: true,
+})
+export class UserComponent {
+  username = 'youngTech';
+}
+
+// @Component({
+//   selector: 'app-root',
+//   template: `
+//   @if (isServerUp){<h1>SERVER IS RUNNING</h1>}
+//   @else{<h1>SERVER IS DEAD</h1>}`,
+//   standalone: true,
+//   imports: [UserComponent],
+// })
+// export class AppComponent {
+//   isServerUp = true
+// }
 @Component({
   selector: 'app-root',
+  template: `
+  @for(user of users; track user.id){
+    <p>{{user.name}}</p>
+  }
+  `,
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [UserComponent],
 })
 export class AppComponent {
-  title = 'myAngular';
+  operatingSystems = [{ id: 'win', name: 'Windows' }, { id: 'osx', name: 'MacOS' }, { id: 'linux', name: 'Linux' }]
+  users = [{ id: 0, name: 'Sarah' }, { id: 1, name: 'Amy' }, { id: 2, name: 'Rachel' }, { id: 3, name: 'Jessica' }, { id: 4, name: 'Poornima' }]
 }
