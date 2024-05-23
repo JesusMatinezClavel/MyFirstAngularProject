f016d081fdebb8ed214b47a646e779fd6c8d4470
JesusMartinezClavel <jmcvalles@gmail.com>
2024-05-23 20:55:22 +0200

    FEAT -> Continued working on angular forms

--- src/app/pages/User/user.component.ts
+++ src/app/pages/User/user.component.ts
@@ -1,14 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 
+import { CustomButton } from "../../common/c-button/cButton.component";
+
 @Component({
   selector: 'app-user',
   styleUrl: './user.component.css',
   templateUrl: './user.component.html',
   standalone: true,
-  imports: [FormsModule]
+  imports: [FormsModule, CustomButton]
 })
 export class UserComponent {
   username = 'YoungTech'
   favoriteFramework = ''
+  showFramework() {
+    alert(this.favoriteFramework)
+  }
 }
