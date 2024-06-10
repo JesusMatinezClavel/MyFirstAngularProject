9aa07a523d6fc8e82bbd268b8371e50c171d1e5f
JesusMartinezClavel <jmcvalles@gmail.com>
2024-06-10 19:53:33 +0200

    FEAT -> Created customCard component

---
+++ src/app/common/c-card/cCard.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+    selector: 'cCard',
+    styleUrl: './cCard.component.css',
+    templateUrl: './cCard.component.html',
+    standalone: true
+})
+export class CustomCard {
+    @Input() children: any
+    @Input() title: string = ''
+}
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,9 +1,14 @@
-import {Component} from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { CustomCard } from "../../common/c-card/cCard.component";
 
 @Component({
   selector: 'app-profile',
-  styleUrl:'./profile.component.css',
-  templateUrl:'./profile.component.html' ,
+  styleUrl: './profile.component.css',
+  templateUrl: './profile.component.html',
   standalone: true,
+  imports: [CustomCard]
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  @Input() test: string = 'hola, que tal?'
+}
