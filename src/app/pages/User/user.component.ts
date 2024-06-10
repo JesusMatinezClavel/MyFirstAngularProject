472c8229290fd297b6b665f4e18cac0d63390e01
JesusMartinezClavel <jmcvalles@gmail.com>
2024-06-10 19:30:30 +0200

    FEAT -> worked on Angular Forms validators and error messages

--- src/app/pages/User/user.component.ts
+++ src/app/pages/User/user.component.ts
@@ -17,14 +17,40 @@ export class UserComponent {
   userEmail = ""
   userPassword = ""
   favoriteFramework = ''
+  errorMsg = ''
   userProfileForm = new FormGroup({
     name: new FormControl('', Validators.required),
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', Validators.required)
   })
-  handleSubmit() {          
-      this.userName = this.userProfileForm.value.name ?? ""
+  logFormErrors() {
+    let stopCheck = false
+    Object.keys(this.userProfileForm.controls).forEach(key => {
+      const controlErrors = this.userProfileForm.get(key)?.errors;
+      if (controlErrors && !stopCheck) {
+        Object.keys(controlErrors).forEach(errorKey => {
+          console.log(errorKey);
+          if (errorKey === 'email') {
+            this.errorMsg = `Invalid email format`
+            stopCheck = true
+            return
+          } else if (key === 'name' || key === 'password' || key === 'email') {
+            this.errorMsg = `${key} required`;
+            stopCheck = true;
+            return;
+          }
+        })
+      }
+    });
+  }
+  handleSubmit() {
+    this.logFormErrors()
+    if (this.userProfileForm.valid) {
+      this.userName = this.userProfileForm.value.name ?? "";
+      this.userPassword = this.userProfileForm.value.password ?? "";
       this.userEmail = this.userProfileForm.value.email ?? ""
-      this.userPassword = this.userProfileForm.value.password ?? ""
+
+      this.errorMsg = `User registered!`
+    }
   }
 }
